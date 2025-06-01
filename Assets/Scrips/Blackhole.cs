7e6f7894b7ee1df5319d5530be4233f4202262c1
Batuhan <batuhan.avci@outlook.de>
2025-06-01 18:26:27 +0200

    Added Blackhole vectorfieldlogic to VFX

--- Assets/Scrips/Blackhole.cs
+++ Assets/Scrips/Blackhole.cs
@@ -1,9 +1,54 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Blackhole : MonoBehaviour
 {
+    [Header("General Values")] //Should be moved to an abstract class
+    [SerializeField] private Vector3 vectorFieldSize = new(5, 0, 5);
+    [SerializeField] private float accelerationDrag = 50;
+    [SerializeField] private float inwardPullStrenght = 0.1f;
+    [SerializeField] private float spiralStrenght = 1;
 
-    public Vector3 BlackHole( Transform foreignTransform, Vector3 velocity, float accelerationDrag = 50, float inwardPullStrenght = 0.1f, float spiralStrenght = 1)
+    [Header("ForeignObject")]
+    [SerializeField] private Transform foreignObject; // Temp until i figure out how to affect units in vectorfield
+    private Vector3 acceleration;
+    private Vector3 velocity;
+
+    // Should be moved to the abstact class
+    // Values to check if a unit is in the vectorfield space
+    private Vector3 vectorFieldBasePositiveCorner;
+    private Vector3 vectorFieldBaseNegativeCorner;
+
+    //The attacked VFX to display the vectorfield
+    private VisualEffect vfxAsset;
+    private float diameter;
+
+    private void Start()
+    {
+        diameter = vectorFieldSize.x + vectorFieldSize.z;
+        vfxAsset = transform.GetComponentInChildren<VisualEffect>();
+        vfxAsset.SetFloat("blackHoleSize", diameter);
+        vfxAsset.enabled = true;
+        //vfxAsset.GetExposedProperties(VFXExposedProperty)
+        vectorFieldBasePositiveCorner = transform.position + vectorFieldSize;
+        vectorFieldBaseNegativeCorner = transform.position - vectorFieldSize;
+        //vfxAsset.GetExposedProperties();
+    }
+
+    private void Update()
+    {
+        var xCheck = foreignObject.position.x <= vectorFieldBasePositiveCorner.x && foreignObject.position.x >= vectorFieldBaseNegativeCorner.x;
+        var zCheck = foreignObject.position.z <= vectorFieldBasePositiveCorner.z && foreignObject.position.z >= vectorFieldBaseNegativeCorner.z;
+        if (xCheck && zCheck)
+        {
+            acceleration = BlackHole(velocity);
+            acceleration -= velocity * 50 * Time.deltaTime;
+            velocity += acceleration * Time.deltaTime;
+            foreignObject.position += velocity * Time.deltaTime;
+        }
+    }
+
+    private Vector3 BlackHole(Vector3 velocity)
     {
         // Fa(x,y) = <-x , -y> inward pull        
         // Fb(x,y) = <+y , -x> vortex
@@ -11,12 +56,13 @@ public class Blackhole : MonoBehaviour
         // F = < -x +y , -y -x> OD < y -x , -x -y> results in the same vectorfield
         float noForceRadius = 0.5f;
 
-        var relativeX = foreignTransform.position.x - transform.position.x;
-        var relativeZ = foreignTransform.position.z - transform.position.z;
+        var relativeX = foreignObject.position.x - transform.position.x;
+        var relativeZ = foreignObject.position.z - transform.position.z;
 
         if (relativeX * relativeX + relativeZ * relativeZ < noForceRadius * noForceRadius)
         {
-            return transform.SpiralVectorField(foreignTransform, new Vector3(50, 0, 50), velocity, accelerationDrag);
+            Debug.Log("Error");
+            return transform.SpiralVectorField(foreignObject, new Vector3(50, 0, 50), velocity, accelerationDrag);
         }
 
         float speed = SpeedCalculator(relativeX, relativeZ, false);
@@ -27,6 +73,7 @@ public class Blackhole : MonoBehaviour
         acceleration -= velocity * accelerationDrag * Time.deltaTime;
         return acceleration;
     }
+
     private float SpeedCalculator(float x, float y, bool sqrt = false)
     {
         float speed = x * x + y * y;
@@ -36,4 +83,9 @@ public class Blackhole : MonoBehaviour
         }
         return speed;
     }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawCube(transform.position, vectorFieldSize * 2);
+    //}
 }
